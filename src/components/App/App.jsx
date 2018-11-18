ec716279ee1428a06d2c8764dd3c1d40c01f31f6
Weaver23PH <themichalcarlo@gmail.com>
2018-11-18 18:50:38 +0100

    added partial contact component, corrected styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,21 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from "./App.scss";
 import SideMenu from '../sidemenu/sidemenu';
+import Contact from '../contact/contact'
 import {HashRouter, Switch, Route} from 'react-router-dom';
 
  class App extends React.Component{
      render(){
+         let style = {
+             position: "fixed",
+         bottom: 0,
+         top: 0,
+         left:0,
+         right: 0
+     };
          return (
-             <React.Fragment>
+
+             <div style={style}>
                 <SideMenu/>
              <HashRouter>
-                     {/*<Switch>*/}
-                         {/*<Route exact path="/" component={MainBody}/>*/}
+                     <Switch>
+                         <Route exact path="/" component={Contact}/>
                          {/*<Route  path="/Rotate" component={Rotate}/>*/}
-                     {/*</Switch>*/}
+                     </Switch>
                  </HashRouter>
-                 }
-             </React.Fragment>
+             </div>
          )
      }
  }
---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from "./contact.scss";
+
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pointerClicked: false
+        }
+
+    }
+
+    handleClick = () => {
+        this.setState({
+            pointerClicked: !this.state.pointerClicked
+        });
+    };
+
+
+
+    render() {
+        return (
+            <div className={styles.map} onClick={this.handleClick}>
+                {this.state.pointerClicked && <div className={styles.mapPopup}>
+                    <div>
+                        <h2>Get in touch with us</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam aspernatur atque,
+                            consequatur dolor, dolorum error facere fugit iusto, pariatur possimus quae quis recusandae
+                            similique sint ut veritatis voluptatem voluptatum.</p>
+                        <h2>Send us a message</h2>
+                        <form>
+                            <div>
+                                <input type="text" value="name" placeholder="Name"/>
+                                <input type="text" value="email" placeholder="email"/>
+                                <input type="text" value="website" placeholder="website"/></div>
+                            <div>
+
+                                <textarea placeholder="Message"/>
+                                <button type="submit" className="styles.submit">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>}
+            </div>
+        )
+    }
+}
+
+export default Contact;
